302ecf9f7979cca936d0d2cb1cfb845ea13394b2
Darkwin123 <nielsvandenbrande123@outlook.com>
2023-11-21 16:21:54 +0100

    player controller

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2023 Generic Asian Name
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
